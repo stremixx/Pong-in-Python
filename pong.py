1a47f7b924bf430c5d0d984ffc0bd2bd972f8999
Ariel Mon Perera <113957960+stremixx@users.noreply.github.com>
2025-05-09 12:00:38 -0400

    Add files via upload

--- pong.py
+++ pong.py
@@ -1,10 +1,13 @@
 import turtle
+import random
+import random
 
 # create window
 window = turtle.Screen()
 window.title("Pong Game")
 window.bgcolor("Black")
 window.setup(width=800, height=600)
+window.cv._rootwindow.resizable(False, False)
 #stop window from updating
 window.tracer(0)
 
@@ -14,7 +17,7 @@ score_b=0
 
 # Paddle a
 paddle_a = turtle.Turtle()
-paddle_a.speed(0)
+paddle_a.speed(5)
 paddle_a.shape("square")
 paddle_a.color("White")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
@@ -34,7 +37,8 @@ paddle_b.goto(350, 0)
 
 # Ball
 ball = turtle.Turtle()
-ball.speed(0)
+# Set ball speed based on a random tick generator
+ball.speed(random.randint(1, 10))
 ball.shape("square")
 ball.color("White")
 ball.penup()
@@ -42,7 +46,7 @@ ball.goto(0, 0)
 
 # Scoring pen
 pen = turtle.Turtle()
-pen.speed(0)
+pen.speed(2)
 pen.color("white")
 pen.penup()
 pen.hideturtle()
@@ -58,25 +62,34 @@ sd.penup()
 sd.shapesize(stretch_wid=50, stretch_len=1)
 
 #ball moving SPEED
-ball.dx = 0.35
-ball.dy = 0.35
+ball.dx = 10 / 30  # Speed adjusted for 30 FPS
+ball.dy = 10 / 30  # Speed adjusted for 30 FPS
 
 # Paddle moving SPEED
 paddle_b.dy = 0.2
 
+# Flags for paddle movement
+paddle_a_up_pressed = False
+paddle_a_down_pressed = False
+
+# Functions to set flags
+def paddle_a_up_press():
+    global paddle_a_up_pressed
+    paddle_a_up_pressed = True
+
+def paddle_a_up_release():
+    global paddle_a_up_pressed
+    paddle_a_up_pressed = False
+
+def paddle_a_down_press():
+    global paddle_a_down_pressed
+    paddle_a_down_pressed = True
+
+def paddle_a_down_release():
+    global paddle_a_down_pressed
+    paddle_a_down_pressed = False
+
 # Functions
-# Going up Paddle a
-def paddle_a_up():
-     y = paddle_a.ycor()
-     y += 10
-     paddle_a.sety(y)
-     
-# Going down paddle a
-def paddle_a_down():
-     y = paddle_a.ycor()
-     y -= 10
-     paddle_a.sety(y)
-       
 # Going up Paddle b
 def paddle_b_up():
      y = paddle_b.ycor()
@@ -92,13 +105,25 @@ def paddle_b_down():
      
 # Keyboard binding
 window.listen()
-window.onkeypress(paddle_a_up, "w")
-window.listen()
-window.onkeypress(paddle_a_down, "s")
+window.onkeypress(paddle_a_up_press, "w")
+window.onkeyrelease(paddle_a_up_release, "w")
+window.onkeypress(paddle_a_down_press, "s")
+window.onkeyrelease(paddle_a_down_release, "s")
 
 #Main game loop
 while True:
      window.update()
+
+     # Move paddle A continuously if keys are pressed
+     if paddle_a_up_pressed:
+         y = paddle_a.ycor()
+         y += 2 #speed of the paddle
+         paddle_a.sety(y)
+
+     if paddle_a_down_pressed:
+         y = paddle_a.ycor()
+         y -= 2 #speed of the paddle
+         paddle_a.sety(y)
      
      # move the ball
      ball.setx(ball.xcor() + ball.dx)
@@ -159,20 +184,22 @@ while True:
           pen.write("{}     {}" .format(score_a, score_b), align="center", font=("Courier", 50, "normal"))
           
           
-     # PAddle and ball collision
-     
-     if (ball.xcor() > 340 and ball.xcor() <350) and (ball.ycor() < paddle_b.ycor() + 40 
-                                                      and ball.ycor() > paddle_b.ycor() -40):
+     # Paddle and ball collision
+
+     # Collision with Paddle B
+     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 
+                                                       and ball.ycor() > paddle_b.ycor() - 50):
           ball.setx(340)
           ball.dx *= -1
-          
-     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 
-                                                      and ball.ycor() > paddle_a.ycor() -40):
+
+     # Collision with Paddle A
+     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 
+                                                         and ball.ycor() > paddle_a.ycor() - 50):
           ball.setx(-340)
-          ball.dx *= -1     
-          
-          
-          
+          ball.dx *= -1
 
 
-     
+
+
+
+
